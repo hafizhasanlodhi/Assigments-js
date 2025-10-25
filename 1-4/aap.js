2c6867fb5913c49fc2e772d59cbd6402b7c727bb
hafizhasanlodhi <hasanlodhi777@gmail.com>
2025-10-25 17:42:27 +0500

    Assjigments

---
+++ 1-4/aap.js
@@ -0,0 +1,19 @@
+//Chapter = 1
+// simpe alert paractice
+alert("I`m Hasan");
+alert("my hubby development");
+
+// var, let , const paractice
+var string = ("Hasan");
+let no = (1, 2, 3) 
+const bolean = (true) 
+
+//document.write() paractice
+var Name = ("Hasan");
+document.write(Name)
+
+//console.log paractice
+var Nam = ("Hasan");
+console.log(Nam);
+
+//Chapter = 2
